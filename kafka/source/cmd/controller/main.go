22f5ad5211f1149e4781a5c49088e4ce3d921fcc
Lukas Berk <lberk@redhat.com>
2019-06-11 13:21:59 -0400

    changes to avoid being lost to time

--- kafka/source/cmd/controller/main.go
+++ kafka/source/cmd/controller/main.go
@@ -18,16 +18,26 @@ package main
 
 import (
 	"log"
+	"os"
 
+	kafkainformer "github.com/knative/eventing-contrib/contrib/kafka/pkg/client/informers/externalversions"
+	"github.com/knative/eventing-contrib/contrib/kafka/pkg/reconciler"
+	kncontroller "github.com/knative/pkg/controller"
+	"github.com/knative/pkg/logging"
+	"github.com/knative/pkg/logging/logkey"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	kubeinformers "k8s.io/client-go/informers"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"knative.dev/eventing-contrib/kafka/source/pkg/apis"
 	controller "knative.dev/eventing-contrib/kafka/source/pkg/reconciler"
 	"knative.dev/pkg/logging/logkey"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+)
+
+const (
+	component = "kafka-controller"
 )
 
 func main() {
@@ -45,28 +55,65 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Setup new signalhandler
+	stopCh := signals.SetupSignalHandler()
+
+	opt := reconciler.NewOptionsOrDie(cfg, logger, stopCh)
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(opt.KubeClientSet, opt.ResyncPeriod)
+	kafkaInformerFactory := kafkainformer.NewSharedInformerFactory(opt.EventingClientSet, opt.ResyncPeriod)
+
+	kafkaInformer := kafkaInformerFactory.Sources().V1alpha1().KafkaSources()
+	deploymentInformer := kubeInformerFactory.Apps().V1().Deployments()
 	// Create a new Cmd to provide shared dependencies and start components
-	mgr, err := manager.New(cfg, manager.Options{})
-	if err != nil {
-		log.Fatal(err)
+
+	controller := kncontroller.Impl{
+		kafka.NewController(
+			opt,
+			kafkaInformer,
+			deploymentInformer,
+		),
 	}
 
-	log.Printf("Registering Components.")
+	// Watch the logging config map and dynamically update logging levels.
+	// par 1 should panic if not set
+	cm := os.Getenv("CONFIG_LOGGING_NAME")
+	opt.ConfigMapWatcher.Watch(cm, logging.UpdateLevelFromConfigMap(logger, zapcore.InfoLevel, "kafka-controller"))
+	// Watch the observability config map and dynamically update metrics exporter.
+	opt.ConfigMapWatcher.Watch(metrics.ObservabilityConfigName, metrics.UpdateExporterFromConfigMap(component, logger))
+	if err := opt.ConfigMapWatcher.Start(stopCh); err != nil {
+		logger.Fatal("failed to start configuration manager", zap.Error(err))
+	}
+
+	logger.Info("Starting informers.")
+	if err := kncontroller.StartInformers(
+		stopCh,
+		kafkaInformer.Informer(),
+		deploymentInformer.Informer(),
+	); err != nil {
+		logger.Fatal("Failed to start informers: %v", err)
+	}
+	kncontroller.StartAll(stopCh, controller)
+	//	mgr, err := manager.New(cfg, manager.Options{})
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+
+	//	log.Printf("Registering Components.")
 
 	// Setup Scheme for all resources
-	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		log.Fatal(err)
-	}
+	//	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
+	//		log.Fatal(err)
+	//	}
 
-	log.Printf("Setting up Controller.")
+	//	log.Printf("Setting up Controller.")
 
 	// Setup Kafka Controller
-	if err := controller.Add(mgr, logger.Sugar()); err != nil {
-		log.Fatal(err)
-	}
+	//	if err := controller.Add(mgr, logger.Sugar()); err != nil {
+	//		log.Fatal(err)
+	//	}
 
-	log.Printf("Starting Apache Kafka controller.")
+	//	log.Printf("Starting Apache Kafka controller.")
 
 	// Start the Cmd
-	log.Fatal(mgr.Start(signals.SetupSignalHandler()))
+	//	log.Fatal(mgr.Start(stopCh))
 }
