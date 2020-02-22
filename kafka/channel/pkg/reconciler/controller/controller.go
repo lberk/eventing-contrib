35d6d628c340f4fd6a62018838914ff468240da3
Lukas Berk <lberk@redhat.com>
2020-02-22 09:38:09 -0500

    Move to ReconcileKind and FinalizeKind

--- kafka/channel/pkg/reconciler/controller/controller.go
+++ kafka/channel/pkg/reconciler/controller/controller.go
@@ -24,14 +24,18 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
 
-	kafkaclientsetinjection "knative.dev/eventing-contrib/kafka/channel/pkg/client/injection/client"
+	kafkaChannelClient "knative.dev/eventing-contrib/kafka/channel/pkg/client/injection/client"
 	"knative.dev/eventing-contrib/kafka/channel/pkg/client/injection/informers/messaging/v1alpha1/kafkachannel"
+	kafkaChannelReconciler "knative.dev/eventing-contrib/kafka/channel/pkg/client/injection/reconciler/messaging/v1alpha1/kafkachannel"
+
+	eventingClient "knative.dev/eventing/pkg/client/injection/client"
 	kubeclient "knative.dev/pkg/client/injection/kube/client"
 	"knative.dev/pkg/client/injection/kube/informers/apps/v1/deployment"
 	"knative.dev/pkg/client/injection/kube/informers/core/v1/endpoints"
 	"knative.dev/pkg/client/injection/kube/informers/core/v1/service"
 
 	"knative.dev/eventing/pkg/reconciler"
+
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
 	"knative.dev/pkg/logging"
@@ -44,62 +48,65 @@ func NewController(
 	ctx context.Context,
 	cmw configmap.Watcher,
 ) *controller.Impl {
-	logger := logging.FromContext(ctx)
-
 	kafkaChannelInformer := kafkachannel.Get(ctx)
 	deploymentInformer := deployment.Get(ctx)
 	endpointsInformer := endpoints.Get(ctx)
 	serviceInformer := service.Get(ctx)
 
-	kafkaChannelClientSet := kafkaclientsetinjection.Get(ctx)
-
 	dispatcherNamespace := system.Namespace()
 
 	r := &Reconciler{
 		Base:                     reconciler.NewBase(ctx, controllerAgentName, cmw),
+		KubeClientSet:            kubeclient.Get(ctx),
+		kafkaClientSet:           kafkaChannelClient.Get(ctx),
+		EventingClientSet:        eventingClient.Get(ctx),
 		dispatcherNamespace:      dispatcherNamespace,
 		dispatcherDeploymentName: dispatcherDeploymentName,
 		dispatcherServiceName:    dispatcherServiceName,
 		kafkachannelLister:       kafkaChannelInformer.Lister(),
-		kafkachannelInformer:     kafkaChannelInformer.Informer(),
-		deploymentLister:         deploymentInformer.Lister(),
-		serviceLister:            serviceInformer.Lister(),
-		endpointsLister:          endpointsInformer.Lister(),
-		kafkaClientSet:           kafkaChannelClientSet,
+		//		kafkachannelInformer:     kafkaChannelInformer.Informer(),
+		deploymentLister: deploymentInformer.Lister(),
+		serviceLister:    serviceInformer.Lister(),
+		endpointsLister:  endpointsInformer.Lister(),
 	}
 
 	env := &envConfig{}
 	if err := envconfig.Process("", env); err != nil {
-		r.Logger.Panicf("unable to process Kafka channel's required environment variables: %v", err)
+		logging.FromContext(ctx).Panicf("unable to process Kafka channel's required environment variables: %v", err)
 	}
 	r.dispatcherImage = env.Image
 
-	r.impl = controller.NewImpl(r, r.Logger, ReconcilerName)
+	impl := kafkaChannelReconciler.NewImpl(ctx, r)
 
 	// Get and Watch the Kakfa config map and dynamically update Kafka configuration.
 	if _, err := kubeclient.Get(ctx).CoreV1().ConfigMaps(system.Namespace()).Get("config-kafka", metav1.GetOptions{}); err == nil {
 		cmw.Watch("config-kafka", r.updateKafkaConfig)
 	} else if !apierrors.IsNotFound(err) {
-		logger.With(zap.Error(err)).Fatal("Error reading ConfigMap 'config-kafka'")
+		logging.FromContext(ctx).With(zap.Error(err)).Fatal("Error reading ConfigMap 'config-kafka'")
 	}
 
-	r.Logger.Info("Setting up event handlers")
-	kafkaChannelInformer.Informer().AddEventHandler(controller.HandleAll(r.impl.Enqueue))
+	logging.FromContext(ctx).Info("Setting up event handlers")
+	kafkaChannelInformer.Informer().AddEventHandler(controller.HandleAll(impl.Enqueue))
+
+	// Call GlobalResync on kafkachannels.
+	globalresyncCh := func(obj interface{}) {
+		impl.GlobalResync(kafkaChannelInformer.Informer())
+	}
 
 	// Set up watches for dispatcher resources we care about, since any changes to these
 	// resources will affect our Channels. So, set up a watch here, that will cause
 	// a global Resync for all the channels to take stock of their health when these change.
 	deploymentInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
 		FilterFunc: controller.FilterWithNameAndNamespace(dispatcherNamespace, dispatcherDeploymentName),
-		Handler:    r,
+		Handler:    controller.HandleAll(globalresyncCh),
 	})
 	serviceInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
 		FilterFunc: controller.FilterWithNameAndNamespace(dispatcherNamespace, dispatcherServiceName),
-		Handler:    r,
+		Handler:    controller.HandleAll(globalresyncCh),
 	})
 	endpointsInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
 		FilterFunc: controller.FilterWithNameAndNamespace(dispatcherNamespace, dispatcherServiceName),
-		Handler:    r,
+		Handler:    controller.HandleAll(globalresyncCh),
 	})
-	return r.impl
+	return impl
 }
